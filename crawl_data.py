2c248d40a4008ed0f5218be3f12f0a68dfc7d29f
Luong <luongnv.grey@gmail.com>
2021-12-25 02:52:38 +0700

    update crawler

--- crawl_data.py
+++ crawl_data.py
@@ -4,24 +4,24 @@ from src.data_helper import DataProcessor
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--batch-size', type=int, default=10, help='batch size')
-    parser.add_argument('-s', '--skiprows', type=int, default=5, help='skip size')
+    parser.add_argument('-b', '--batch-size', type=int, default=100, help='batch size')
+    parser.add_argument('-s', '--skiprows', type=int, default=0, help='skip size')
     parser.add_argument('-d', '--data-dir', type=str, default='data', help='data directory')
     parser.add_argument('-o', '--output-dir', type=str, default='data', help='output directory')
-    parser.add_argument('-t', '--type', type=str, default='test', help='data type')
+    parser.add_argument('-t', '--type', type=str, default='train', help='data type')
     parser.add_argument('-f', '--first-batch', type=int, default=0, help='first batch')
     args = parser.parse_args()
     return args
     
 def main():
     args = parse_args()
-    file_dir = "input/data/images_2017_11/2017_11/" + args.type + "/"
+    file_dir = "input/2017_11/" + args.type + "/"
     # file_dir = "input/data/stl10_binary/"
     file_name = "images.csv"
     DataProcessor.read_url_csv(file_dir, file_name, 
                                chunksize=args.batch_size, 
                                skiprows=args.skiprows,
-                               first_batch=1, 
+                               first_batch=args.first_batch, 
                                only_save_img=True)
     
 if __name__ == "__main__":
