4066027444e8a6a5eb49d563fa303786ab554d1c
Vu Quoc Hien <NeiH4207@gmail.com>
2022-03-09 04:54:50 +0700

    update param

--- train_model.py
+++ train_model.py
@@ -15,31 +15,19 @@ import cv2
 from utils import *
 
 def get_dataset(file_dir, file_name, iter, saved=False):
-    if configs['preprocess']:
-        dataset = None
-        dataset = DataProcessor.load_data_from_binary_file(file_dir, file_name)
-        dataset = DataProcessor.generate_data(dataset, 
-                                        (2, 2),
-                                        img_configs['block-size'],
-                                        n_jobs=4)
-        
-        DataProcessor.save_data_to_binary_file(dataset, "input/data/64x64/train_batchs/dataset_{}.bin".format(iter))
-        trainset, testset = DataProcessor.split_dataset(dataset, 0.98, saved=False)
-    else:
-        dataset = DataProcessor.load_data_from_binary_file("input/data/64x64/train_batchs/","dataset_{}.bin".format(iter))
-        trainset, testset = DataProcessor.split_dataset(dataset, 0.99, saved=False)
+    dataset = DataProcessor.load_data_from_binary_file("input/data/64x64/train_batchs/","dataset_{}.bin".format(iter))
+    trainset, testset = DataProcessor.split_dataset(dataset, 0.99, saved=False)
     return trainset, testset    
 
 def main():
-    configs['preprocess'] = False
     configs['num-dataset'] = 200
     file_dir = "input/data/64x64/images/"
     trainer = Trainer(model=VGG('VGG7'), 
-                      lr=0.0001, 
+                      lr=0.003, 
                       loss='bce', 
                       optimizer='adas', 
                       batch_size=256, 
-                      n_repeats=2,
+                      n_repeats=3,
                       save_every=1000
                       )
     # trainer.model.load(1, 908)
@@ -53,9 +41,9 @@ def main():
         sample_img = trainset['data'][_id][0]
         print('target: {}'.format(trainset['target'][_id]))
         cv2.imwrite("output/sample.png", sample_img)
-        trainer.train_loader = trainset
-        trainer.test_loader = trainset
-        trainer.test()
+        trainer.train_loader = testset
+        trainer.test_loader = testset
+        trainer.train()
         trainer.train_loader = None
         trainer.test_loader = None
         trainset, testset = None, None
